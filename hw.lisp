95c1d7e1c2d7f22b02f847730891023a909a13c8
Pascal J. Bourguignon <pjb@informatimago.com>
2020-10-18 01:27:20 +0200

    Added docstrings.

--- hw.lisp
+++ hw.lisp
@@ -1,14 +1,18 @@
 (defpackage "HELLO-WORLD"
   (:use "COMMON-LISP")
-  (:export "HW" "MAIN"))
+  (:export "HW" "MAIN")
+  (:documentation "Implements the Hello, World! hw function, and main function.
+Copyright 2020 Pascal J. Bourguignon <pjb@informatimago.com>"))
 (in-package "HELLO-WORLD")
 
 (defun hw (&optional (out *standard-output*))
+  "Prints out a Hello, World! message."
   (format out "~&Hello, World!~%")
   (finish-output out)
   (values))
 
 (defun main (pname &rest arguments)
+  "The main function for a hw executable."
   (declare (ignore pname arguments))
   (hw))
 
