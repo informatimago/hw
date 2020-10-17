2b1ffe2e027c6be8390719b4d4e12a008a64dc76
Pascal J. Bourguignon <pjb@informatimago.com>
2020-10-18 01:58:32 +0200

    Corrected hw message.

--- hw-ecl.c
+++ hw-ecl.c
@@ -4,7 +4,7 @@ int main(int argc, char **argv)
 {
     cl_boot(argc, argv);
     cl_eval(c_string_to_object("(defun hw ()"
-                               "  (write-line \"Hello World!\")"
+                               "  (write-line \"Hello, World!\")"
                                "  (values))"));
     cl_eval(c_string_to_object("(hw)"));
     cl_shutdown();
